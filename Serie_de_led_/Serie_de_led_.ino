cd07ebaeaca59fe86d4dd2834a5115ea11c35f07
laparrah <lauranelly.parra@gmail.com>
2018-04-22 18:11:30 -0500

    agregamos un led mas

--- Serie_de_led_/Serie_de_led_.ino
+++ Serie_de_led_/Serie_de_led_.ino
@@ -1,10 +1,12 @@
 int led1 = 2;
 int led2 = 3;
 int led3 = 4; 
+int led4 = 5;
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
   Serial.begin(9600);
   // put your setup code here, to run once:
 
@@ -21,6 +23,11 @@ void loop() {
   delay(1000);
   digitalWrite(led3, HIGH);
   delay(1000);
-  digitalWrite(led1, LOW);
+  digitalWrite(led3, LOW);
   delay(1000);
+  digitalWrite(led4, HIGH);
+  delay(1000);
+  digitalWrite(led4, LOW);
+  delay(1000);
+  
 }
