a2c6fb0f873c524521a7b23957834cfea339bf22
laparrah <38633312+laparrah@users.noreply.github.com>
2018-04-22 18:24:48 -0500

    Empeazamos a crear una serie mas grande

--- Serie_de_led_/Serie_de_led_.ino
+++ Serie_de_led_/Serie_de_led_.ino
@@ -2,11 +2,13 @@ int led1 = 2;
 int led2 = 3;
 int led3 = 4; 
 int led4 = 5;
+int led5 = 6;
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
   Serial.begin(9600);
   // put your setup code here, to run once:
 
@@ -28,6 +30,10 @@ void loop() {
   digitalWrite(led4, HIGH);
   delay(1000);
   digitalWrite(led4, LOW);
+  delay(1000);
+  digitalWrite(led5, HIGH);
+  delay(1000);
+  digitalWrite(led5, LOW);
   delay(1000);
   
 }
